6a14f4cfcbcf9a7f031bfbc3f617d6c58b900969
Marsileno_Medhat <marsilenomedhat01@gmail.com>
2026-01-07 14:31:10 +0200

    core projects file.

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+using namespace std;
+
+int main() {
+    while(true) {
+        clsLoginScreen::ShowLoginScreen();  
+    }
+    system("pause>0");
+    return 0;
+}
