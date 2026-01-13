e378c6efcd60bca3913c0c0cc7f6356041d05cc6
Marsileno_Medhat <marsilenomedhat01@gmail.com>
2026-01-13 23:00:16 +0200

    create transfer feature

---
+++ clsTransferScreen.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "clsScreen.h"
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include "clsUtil.h"
+using namespace std;
+
+class clsTransferScreen : protected clsScreen {
+
+private:
+
+    static void _PrintClientCard(clsBankClient Client, string ClientType) {
+        cout << "\n_________________________________\n";
+        cout << "\n\t" << ClientType << " Card";
+        cout << "\n_________________________________\n";
+        cout << "\nFull name  : " << Client.GetFullName();
+        cout << "\nAcc number : " << Client.GetAccountNumber();
+        cout << "\nBalance    : " << Client.GetAccountBalance();
+        cout << "\n_________________________________\n";
+    }
+
+    static string _ReadAccountNumber(string ClientType) {
+        string AccountNumber = "";
+        while (true) {
+            string Message = "\nEnter the " + ClientType + " account number or enter \"back\" to back to the transactions menu: ";
+            AccountNumber = clsInputValidate::ReadString(Message);
+
+            if (clsUtil::IsEqualText(AccountNumber, "back")) {
+                return "back";
+            }
+
+            if (clsBankClient::IsClientExist(AccountNumber)) {
+                return AccountNumber;
+            }
+            cout << "\nAccount number is not found. ";
+        }
+    }
+
+public:
+
+    static void ShowTransferScreen() {
+        clsScreen::_PrineScreenHeader("Transfer Screen");
+
+        string SenderAccountNumber = _ReadAccountNumber("sender");
+        if(SenderAccountNumber == "back") {
+            cout << "\nTransaction has been cancelled.\n";
+            return;
+        }
+        clsBankClient Sender = clsBankClient::Find(SenderAccountNumber);
+        _PrintClientCard(Sender, "Sender");
+        
+        string ReceiverAccountNumber = _ReadAccountNumber("receiver");
+        if(ReceiverAccountNumber == "back") {
+            cout << "\nTransaction has been cancelled.\n";
+            return;
+        }
+        clsBankClient Receiver = clsBankClient::Find(ReceiverAccountNumber);
+        _PrintClientCard(Receiver, "Receiver");
+        
+        double TransferAmount = 0;
+
+        while (true) {
+            TransferAmount = clsInputValidate::ReadPositiveValidNumber<float>("\nEnter the withdraw amount or enter 0 to cancel the transaction: ", "\nInvalid number, Enter the withdraw amount or enter 0 to cancel the transaction: ", "Invalid number. ", true);
+            if (TransferAmount == 0) {
+                cout << "\nThe transaction has been cancelled.\n";
+                return;
+            }
+            if (TransferAmount <= Sender.GetAccountBalance()) {
+                break;
+            }
+            cout << "\nThe withdraw amount exceeds the current balance.\n";
+        }
+
+        char ConfirmTransaction = clsInputValidate::ReadChar("\nDo you want to process this transaction? [Y/N]:  ");
+        
+        if (toupper(ConfirmTransaction) == 'Y') {
+            if (Sender.Transfer(Receiver, TransferAmount)) {
+                _PrintClientCard(Sender, "Sender");
+                _PrintClientCard(Receiver, "Receiver");
+                cout << "\nTransaction has been done successfully.\n";
+            }
+        }
+        else {
+            cout << "\nTransaction has been cancelled.\n";
+        }
+    }
+
+};
